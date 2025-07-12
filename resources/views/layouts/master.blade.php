dcf6d6c6e8b0cb06f58e5d9a035fcea38edaed60
AMK1215 <aungmyokhaing1215@gmail.com>
2025-07-12 13:27:00 +0630

    upate logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GSC PLUS | Dashboard</title>
+    <title>TTTGaming | Dashboard</title>
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -621,7 +621,7 @@
             @yield('content')
         </div>
         <footer class="main-footer">
-            <strong>Copyright &copy; 2025 <a href="">GSC PLUS</a>.</strong>
+            <strong>Copyright &copy; 2025 <a href="">TTTGaming</a>.</strong>
             All rights reserved.
             <div class="float-right d-none d-sm-inline-block">
                 <b>Version</b> 3.2.2
